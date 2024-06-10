74b782c6f6737eb0c6e6c908463c442f1f893162
Barrack Ha <barakiey@gmail.com>
2024-06-10 09:36:36 +0300

    some code for the sol

--- ex2/sol2.py
+++ ex2/sol2.py
@@ -49,5 +49,84 @@ def q_2():
 
 
 if __name__ == "__main__":
-    q_1()
-    q_2()
+    # q_1()
+    # q_2()
+    # fig, ax = plt.subplots()
+    import numpy as np
+    from scipy.integrate import solve_ivp
+    import matplotlib.pyplot as plt
+
+    # Define the system of ODEs
+    def linear_system(t, h, M, h_0):
+        """
+        The system of ODEs for the linear system of equations.
+        h = [h_E, h_I]
+        h_0 = [h_E0, h_I0] is the external input.
+        """
+        return (M @ h) + h_0
+
+    # Define the 2x2 matrix A
+    M = np.array([[-1, 1],
+                [0, -1]])
+    
+    J_EE, J_EI = 2, 2
+    M_1 = np.array([
+        [(J_EE - 1), (-1 * J_EI)],
+        [1, -1]
+    ])
+
+    M_3 = np.array([
+        [-1, -1*J_EI],
+        [0, -1]
+    ])
+
+    # 4. (h_E < 0) && (0 < h_I)
+    # h_ext = [h_E^0, h_I^0]
+    h_0 = np.array([-10, -10])
+    # Initial conditions
+    y0 = [6, 2]
+    NPOINTS = 1000
+    # Time span for the simulation
+    t_span = (0, 100)
+    t_eval = np.linspace(t_span[0], t_span[1], NPOINTS)
+
+    # Solve the system of ODEs
+    solution = solve_ivp(linear_system, t_span, y0, args=(M_1, h_0), t_eval=t_eval)
+
+    # Plot the solution
+    fig, ax = plt.subplots()
+    
+    cmap = plt.cm.get_cmap('Blues', NPOINTS)
+    colorpoints = np.linspace(0, 1, NPOINTS)
+    sc = ax.scatter(solution.y[0], solution.y[1], c=colorpoints, cmap=cmap)
+    # Add a color bar
+    cbar = fig.colorbar(sc, ax=ax)
+    cbar.set_label('Time')
+
+    ax.set_xlabel('y_0')
+    ax.set_ylabel('y_1')
+    # ax.legend()
+    # ax.suptitle('Solution of the Coupled 2D Linear System')
+    ax.grid()
+    # plt.show()
+
+    fig_fft, (r_vs_t, psd) = plt.subplots(2)
+    r_vs_t.plot(solution.t, solution.y[0], label=r'$r_{E}$')
+
+    # Run numpy fft on solution.y[0]
+    print(solution.y[0].shape)
+    fft = np.fft.fft(solution.y[0])
+    print(fft.shape)
+    # freqs = np.fft.fftfreq(len(fft), d=1/NPOINTS)
+    freqs = np.fft.fftfreq(len(fft), d=1/10)
+    print(freqs.shape)
+    # Plot the power spectrum
+    psd.plot(freqs[:len(fft)//2], np.abs(fft)[:len(fft)//2])
+    # psd.plot(np.abs(fft))
+    # psd.set_xlabel('Frequency')
+    # psd.set_ylabel('Power')
+    # psd.legend()
+    # psd.grid()
+
+
+    plt.show()
