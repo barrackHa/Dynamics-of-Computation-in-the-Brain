b652e065958bcb4974cf55291dcf774178d8637c
Barrack Ha <barakiey@gmail.com>
2024-06-12 08:18:38 +0300

    Added a new sim method and add func to q_3

--- ex2/sol2.py
+++ ex2/sol2.py
@@ -42,6 +42,35 @@ def M_3(J_EI=2):
         [-1, -1]
     ])
 
+def excitatory_inhibitor_system(t, h, J_EE=2, J_EI=1, h_0=[2,1]):
+    """
+    The system of ODEs for the excitatory-inhibitory system.
+    h = [h_E, h_I]
+    """
+    # simple Relu function 
+    h_E_p = h[0] * (h[0] > 0)
+    h_I_p = h[1] * (h[1] > 0)
+
+    h_E_0, h_I_0 = h_0
+
+    dh_E = (-1 * h_E_p) + (J_EE * h_E_p) - (J_EI * h_I_p) + h_E_0
+    dh_I = (-1 * h_I_p) + h_E_p + h_I_0
+    
+    return np.array([dh_E, dh_I])
+
+def simulate_excitatory_inhibitory_system(
+    J_EE=2, J_EI=1, h_0=[2,1], initial_conditions=[1,1], time_span=(0, 100), points_num=100
+):
+    """
+    Simulate the excitatory-inhibitory system.
+    """
+    t_eval = np.linspace(time_span[0], time_span[1], points_num)
+    solution = solve_ivp(
+        excitatory_inhibitor_system, time_span, initial_conditions, 
+        args=(J_EE, J_EI, h_0), t_eval=t_eval
+    )
+    return solution
+
 def get_simulation_results(Mat, external_input, initial_conditions, time_span, points_num):
     t_eval = np.linspace(time_span[0], time_span[1], points_num)
     solution = solve_ivp(
@@ -50,7 +79,10 @@ def get_simulation_results(Mat, external_input, initial_conditions, time_span, p
     )
     return solution
 
-def q_2_plot_helper(ax, h_E_0, h_I_0, J_EE, J_EI, t_span, NPOINTS, fig=None, e_type=''):
+def q_2_plot_helper(
+        ax, h_E_0, h_I_0, J_EE, J_EI, t_span, NPOINTS, 
+        fig=None, e_type='', use_linear_system=False
+    ):
 
     # Verify the fixed point conditions
     A = (0 < 1 - J_EE + J_EI) 
@@ -76,7 +108,12 @@ def q_2_plot_helper(ax, h_E_0, h_I_0, J_EE, J_EI, t_span, NPOINTS, fig=None, e_t
     # Solve the system of ODEs
     for a,b in [[1,1], [-1,1], [1,-1], [-1,-1]]:
         y0 = [(h_E_star + a*dx), (h_I_star + b*dy)]
-        solution = get_simulation_results(M_1(J_EE,J_EI), h_0, y0, t_span, NPOINTS)
+
+        if use_linear_system:
+            solution = get_simulation_results(M_1(J_EE,J_EI), h_0, y0, t_span, NPOINTS)
+        else: 
+            solution = simulate_excitatory_inhibitory_system(J_EE, J_EI, h_0, y0, t_span, NPOINTS)
+        
         # Plot the solution
         ax.plot(solution.y[0], solution.y[1], label=f'Starting At ({y0[0]:.2f},{y0[1]:.2f})')
         sc = ax.scatter(solution.y[0], solution.y[1], c=solution.t, cmap='viridis', alpha=0.7)
@@ -111,7 +148,8 @@ def q_2_plot_helper(ax, h_E_0, h_I_0, J_EE, J_EI, t_span, NPOINTS, fig=None, e_t
     # ax.set_ylim([-30, 30])
     return fig, ax
 
-def plot_helper_wrapper(Js_array, h_E_0=2, h_I_0=1, t_span=(0, 15), NPOINTS=1000, type_str=''):
+def plot_helper_wrapper(Js_array, h_E_0=2, h_I_0=1, t_span=(0, 15), 
+        NPOINTS=1000, type_str='', use_linear_system=False):
     fig, axs = plt.subplots(1, Js_array.shape[0], figsize=(14,8))
 
     if Js_array.shape[0] == 1:
@@ -119,7 +157,10 @@ def plot_helper_wrapper(Js_array, h_E_0=2, h_I_0=1, t_span=(0, 15), NPOINTS=1000
     
     for i, (J_EE, J_EI) in enumerate(Js_array):
         fig_obj = None if (i<Js_array.shape[0]-1) else fig
-        q_2_plot_helper(axs[i], h_E_0, h_I_0, J_EE, J_EI, t_span, NPOINTS, fig=fig_obj, e_type=type_str)
+        q_2_plot_helper(
+            axs[i], h_E_0, h_I_0, J_EE, J_EI, t_span, NPOINTS, 
+            fig=fig_obj, e_type=type_str, use_linear_system=use_linear_system
+        )
     
     return {
         'fig': fig,
@@ -130,51 +171,59 @@ def plot_helper_wrapper(Js_array, h_E_0=2, h_I_0=1, t_span=(0, 15), NPOINTS=1000
         'initial_conditions': np.array(Js_array),
     }
 
-def q_2_2_1_real_stable():
+def q_2_2_1_real_stable(use_linear_system=False):
     J_EE, J_EI = 1, 0.2 # Stable 1
     J_EE_2, J_EI_2 = 1.1, 0.25 # Stable 2
     return plot_helper_wrapper(np.array([
-        [J_EE, J_EI],
-        [J_EE_2, J_EI_2]
-    ]), type_str='Real Stable')
+            [J_EE, J_EI],
+            [J_EE_2, J_EI_2]
+        ]), type_str='Real Stable',
+        use_linear_system=use_linear_system
+    )
     
 
-def q_2_2_1_real_unstable():
+def q_2_2_1_real_unstable(use_linear_system=False):
     t_span, NPOINTS = (0, 5), 100
     J_EE, J_EI = 4, 2.2 # unStable 1
     J_EE_2, J_EI_2 = 4.1, 2.3 # unStable 2
     
     return  plot_helper_wrapper(np.array([
-        [J_EE, J_EI],
-        [J_EE_2, J_EI_2]
-    ]), t_span=t_span, NPOINTS=NPOINTS, type_str='Real UnStable')
+            [J_EE, J_EI],
+            [J_EE_2, J_EI_2]
+        ]), t_span=t_span, NPOINTS=NPOINTS, type_str='Real UnStable',
+        use_linear_system=use_linear_system
+    )
 
-def q_2_2_1_complex_stable():
+def q_2_2_1_complex_stable(use_linear_system=False):
     J_EE, J_EI = 0.6, 0.9 # Complex Stable 1
     J_EE_2, J_EI_2 = 1.5, 0.85 # Complex Stable 2
     return plot_helper_wrapper(np.array([
-        [J_EE, J_EI],
-        [J_EE_2, J_EI_2]
-    ]), type_str='Complex Stable')
+            [J_EE, J_EI],
+            [J_EE_2, J_EI_2]
+        ]), type_str='Complex Stable', 
+        use_linear_system=use_linear_system
+    )
 
-def q_2_2_1_complex_unstable():
-    J_EE, J_EI = 2.5, 1.75 # Complex unStable 1
+def q_2_2_1_complex_unstable(use_linear_system=False):
+    J_EE, J_EI = 2.1, 1.25 # Complex unStable 1
+    # J_EE, J_EI = 2.5, 1.75 # Complex unStable 1
     J_EE_2, J_EI_2 = 2.6, 1.9 # Complex unStable 2
     return plot_helper_wrapper(np.array([
-        [J_EE, J_EI],
-        [J_EE_2, J_EI_2]
-    ]), type_str='Complex UnStable')
+            [J_EE, J_EI],
+            [J_EE_2, J_EI_2]
+        ]), type_str='Complex UnStable', t_span=(0, 15), NPOINTS=1000,
+        use_linear_system=use_linear_system
+    )
 
 def q_2():
     return [
         q_2_2_1_real_stable(),
         q_2_2_1_real_unstable(),
         q_2_2_1_complex_stable(),
-        q_2_2_1_complex_unstable()
+        q_2_2_1_complex_unstable(),
     ]
 
-def q_3_1(J_EE=2, J_EI=2, t_span=(0, 100), NPOINTS=100, h_0=np.array([2, 1])):    
-    y0 = [1, 1]
+def q_3_1_1(J_EE=2, J_EI=2, t_span=(0, 100), NPOINTS=100, h_0=np.array([2, 1]), y0=[1,1]):    
     
     solution = get_simulation_results(M_1(J_EE,J_EI), h_0, y0, t_span, NPOINTS)
     fig, ax = plt.subplots()
@@ -187,9 +236,9 @@ def q_3_1(J_EE=2, J_EI=2, t_span=(0, 100), NPOINTS=100, h_0=np.array([2, 1])):
 
     return fig, ax, solution
 
-def q_3_2_1(sol=None, t_span=(0, 100), NPOINTS=100):
+def q_3_1_2(sol=None, t_span=(0, 100), NPOINTS=100):
     if sol is None:
-        _, _, sol = q_3_1(t_span=t_span, NPOINTS=NPOINTS)
+        _, _, sol = q_3_1_1(t_span=t_span, NPOINTS=NPOINTS)
     
     # Run scipy fft on solution.y[0]
     r_E_fft = fft(sol.y[0])
@@ -211,23 +260,61 @@ def q_3_2_1(sol=None, t_span=(0, 100), NPOINTS=100):
     psd.legend()
     psd.set_facecolor('lightgray')
 
-def q_3_2_4():
+def q_3_1_2_4():
+    funcs = []
+    y0 = np.array((2.5, 3.5))
+    h_0 = np.array([36, 4])
+    for J_EE in [2,2.5,3]:
+        func = []
+        # b = 2 + i
+        # for J_EE, J_EI in [[b, 1.75], [b, 1.25], [b, 4], [b, 16], [b,18]]:
+        for J_EI in np.linspace(2, 7, 20):
+        # for J_EE, J_EI in [[2, 1.1], [2.5, 1.7], [3, 2.5]]:
+            sol = get_simulation_results(
+                M_1(J_EE,J_EI), initial_conditions=y0, 
+                external_input=h_0, time_span=(0, 100), points_num=100
+            )
+            # _, _, sol = q_3_1_1(J_EE, J_EI, h_0=np.array([6, 2]), y0=y0)
+            # sig.plot(sol.t, sol.y[0], label=f'J_EE={J_EE}, J_EI={J_EI}')
+            r_E_fft = fft(sol.y[0])
+            freqs = fftfreq(len(r_E_fft), d=1)
+            # psd.plot(freqs[0<=freqs], np.abs(r_E_fft)[0<=freqs], label=f'J_EE={J_EE}, J_EI={J_EI}')
+            max_freq = freqs[np.abs(r_E_fft)[0.05<freqs].argmax()]
+            func.append(np.array([J_EI, max_freq]))
+            # print(func)
+        funcs.append(np.array(func))
     
-    # plt.cla()
-    # plt.clf()
+    funcs = np.array(funcs)
+    print(funcs.shape)
 
-    plt, (sig, psd) = plt.subplots(2)
-    for J_EE, J_EI in [[1, 0.2], [4, 2.2], [0.6, 0.9], [2.5, 1.75]]:
-        _, _, sol = q_3_1()
-        sig.plot(sol.t, sol.y[0], label=f'J_EE={J_EE}, J_EI={J_EI}')
+    fig, ax = plt.subplots()
+    for func in funcs:
+        ax.plot(func[:,0], func[:,1])
+
+    plt.show()
+    exit()
+
+    # fig, (sig, psd) = plt.subplots(2)
+    # sig.set_xlabel('Time [Seconds]')
+    # sig.set_ylabel(r'$r_{E}$', rotation=0)
+    # sig.set_facecolor('lightgray')
+    # sig.grid()
+    # sig.legend()
+    # psd.set_xlabel('Frequency [Hz]')
+    # psd.set_ylabel('Power')
+    # psd.set_title(r'Power Spectrum Of $r_{E}$')
+    # psd.grid()
+    # psd.legend()
+    # psd.set_facecolor('lightgray')
+
     
 
 
 def q_3():
     # t_span, NPOINTS = (0, 100), 100
-    # _, _, sol_q_3_1 = q_3_1()
-    # q_3_2_1()
-    q_3_2_4()
+    # _, _, sol_q_3_1 = q_3_1_1()
+    q_3_1_2()
+    # q_3_1_2_4()
 
 if __name__ == "__main__":
 
